bc3e14b3c8e6eae91d3bdd71c4de81031d18024e
xjoseduran01 <142642717+xjoseduran01@users.noreply.github.com>
2024-06-30 11:20:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ from flask_sqlalchemy import SQLAlchemy
 from config import DATABASE_CONNECTION_URI
 from flask_cors import CORS
 from utils.db import db
-from flask_babel import Babel
+
 app = Flask(__name__)
-babel = Babel(app)
+
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.secret_key = 'clavesecreta123'
@@ -27,7 +27,7 @@ app.config["SQLALCHEMY_POOL_RECYCLE"] = 1800
 
 # no cache
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-app.config['BABEL_DEFAULT_LOCALE'] = 'es'
+
 
 db.init_app(app)
 
